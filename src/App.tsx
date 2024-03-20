5351c934644520c48092fd83c275aabab4c6394d
gyojinnK <ksmug1998@gmail.com>
2024-03-20 18:28:16 +0900

    :adhesive_bandage: Simple fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import topPUI from "./assets/topPhoneUI.png";
+import Header from "./components/header/Header.tsx";
 
-function App() {
-    return <></>;
-}
+const App: React.FC = () => {
+    return (
+        <div className="mainWrap">
+            <img src={topPUI} alt="topUI" />
+            <Header />
+        </div>
+    );
+};
 
 export default App;
