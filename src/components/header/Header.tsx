90295b55478bcdec223877bdfc2dd95278691739
gyojinnK <ksmug1998@gmail.com>
2024-03-20 18:25:41 +0900

    :memo: Add files
    상단 header 컴포넌트 생성

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import "./Header.css";
+import Navbar from "./Navbar";
+
+const Header: React.FC = () => {
+    return (
+        <div className="headerWrap">
+            <Navbar />
+        </div>
+    );
+};
+
+export default Header;
