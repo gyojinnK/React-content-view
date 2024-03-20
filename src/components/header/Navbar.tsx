1ded4c9d16ac51ff51c835db7d54bd70e922f6e3
gyojinnK <ksmug1998@gmail.com>
2024-03-20 18:26:32 +0900

    :memo: Add files
    header 내부 상단 메뉴바(navbar) 생성

---
+++ src/components/header/Navbar.tsx
@@ -0,0 +1,28 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+    return (
+        <ul>
+            <li>
+                <a href="">차트</a>
+            </li>
+            <li>
+                <a href="">Whook</a>
+            </li>
+            <li>
+                <a href="">이벤트</a>
+            </li>
+            <li>
+                <a href="">뉴스</a>
+            </li>
+            <li>
+                <a href="">스토어</a>
+            </li>
+            <li>
+                <a href="">충전소</a>
+            </li>
+        </ul>
+    );
+};
+
+export default Navbar;
