e75e1e32017b5050a4d2201931c7e48d7e2ae923
gyojinnK <ksmug1998@gmail.com>
2024-03-21 20:12:32 +0900

    :label: Add type
    BannerInfo type 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+type BannerInfo = {
+    id: number;
+    imagePath: string;
+    title: string;
+    description: string;
+    externalURL: string;
+    period: string;
+    category: string;
+};
