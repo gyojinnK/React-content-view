63b54a51689b84dec38b2f3258ea3cb332dbbe21
gyojinnK <ksmug1998@gmail.com>
2024-03-21 15:31:10 +0900

    :memo: Update file
    simple fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import topPUI from "./assets/topPhoneUI.png";
-import Header from "./components/header/Header.tsx";
+import topPUI from "./assets/images/topPhoneUI.png";
+import Header from "./components/layouts/Header.tsx";
 
 const App: React.FC = () => {
     return (
