b4cb6bbc21e99a699ea2cfce33ac9bb83e2edd7a
gyojinnK <ksmug1998@gmail.com>
2024-03-21 22:21:44 +0900

    :memo: Update file
    내버 좌우측 가장자리에 blur 요소 추가

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -5,6 +5,8 @@ const Article = () => {
     return (
         <div className="articleWrap">
             <BannerWrap />
+            <div className="lBlur"></div>
+            <div className="rBlur"></div>
         </div>
     );
 };
