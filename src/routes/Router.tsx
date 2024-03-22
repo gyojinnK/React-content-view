d514cae4f853931fd6de594afc30d0671de127c2
gyojinnK <ksmug1998@gmail.com>
2024-03-22 17:35:11 +0900

    :memo: Update file
    QueryClient, Provider 적용

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -6,19 +6,23 @@ import NewsPage from "../pages/NewsPage";
 import StorePage from "../pages/StorePage";
 import ChargePage from "../pages/ChargePage";
 import App from "../App";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const Router = () => {
+    const queryClient = new QueryClient();
     return (
         <BrowserRouter>
             <App>
-                <Routes>
-                    <Route path="/" element={<ChartPage />} />
-                    <Route path="/whook" element={<WhookPage />} />
-                    <Route path="/event" element={<EventPage />} />
-                    <Route path="/news" element={<NewsPage />} />
-                    <Route path="/store" element={<StorePage />} />
-                    <Route path="/charge" element={<ChargePage />} />
-                </Routes>
+                <QueryClientProvider client={queryClient}>
+                    <Routes>
+                        <Route path="/" element={<ChartPage />} />
+                        <Route path="/whook" element={<WhookPage />} />
+                        <Route path="/event" element={<EventPage />} />
+                        <Route path="/news" element={<NewsPage />} />
+                        <Route path="/store" element={<StorePage />} />
+                        <Route path="/charge" element={<ChargePage />} />
+                    </Routes>
+                </QueryClientProvider>
             </App>
         </BrowserRouter>
     );
