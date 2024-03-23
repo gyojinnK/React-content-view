7ec1ba4ca0a7ad56516d0441aff6b16fc0685515
gyojinnK <ksmug1998@gmail.com>
2024-03-23 22:35:40 +0900

    :memo: Update file
    외부 링크 연결 및 레이아웃 수정 작업

--- src/components/section/Card.tsx
+++ src/components/section/Card.tsx
@@ -1,28 +1,65 @@
 import Box from "../ui/Box";
-import "./Card.css";
+import ChartSt from "./styles/Chart.module.css";
+import NewsSt from "./styles/News.module.css";
 
 const Card: React.FC<CardInfo> = ({
     id,
-    contentImg,
-    contentTitle,
-    contentDescription,
+    image,
+    title,
+    description,
     views,
+    url,
+    categoryTitle,
 }) => {
+    const toExternalUrlHandler = () => {
+        window.open(url);
+    };
+
+    let css: CSSModuleClasses;
+    let boxStyle = {};
+    switch (categoryTitle) {
+        case "ARTIST":
+            css = ChartSt;
+            boxStyle = { height: "64px" };
+            break;
+        case "NEWS":
+            css = NewsSt;
+            boxStyle = { height: "120px" };
+            break;
+        default:
+            css = ChartSt;
+            boxStyle = { height: "64px" };
+    }
+
     return (
-        <Box className="CardWrap" width="90%" height="64px" margin="10px auto">
-            <img
-                className="contentImg"
-                src={contentImg}
-                alt={`${contentTitle} image`}
-            />
-            <div className="contentDescription">
-                <p>{id}</p>
-                <div className="contentDomain">
-                    <p>{contentTitle}</p>
-                    <p>{contentDescription}</p>
+        <Box
+            className={css.cardWrap}
+            width="90%"
+            margin="10px auto"
+            {...boxStyle}
+        >
+            <div className={css.imgWrap}>
+                {categoryTitle === "NEWS" ? (
+                    <div className={css.blackBoard}></div>
+                ) : null}
+                <img
+                    className={css.contentImg}
+                    src={image}
+                    alt={`${title} image`}
+                />
+            </div>
+            <div className={css.contentDescription}>
+                {categoryTitle === "ARTIST" ? <p>{id}</p> : null}
+                <div className={css.contentDomain}>
+                    {categoryTitle === "NEWS" ? (
+                        <p onClick={toExternalUrlHandler}>{title}</p>
+                    ) : (
+                        <p>{title}</p>
+                    )}
+                    <p>{description}</p>
                 </div>
             </div>
-            <p className="views">{views}</p>
+            <p className={css.views}>{views}</p>
         </Box>
     );
 };
