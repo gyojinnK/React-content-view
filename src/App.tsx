aec0692a667e384a21309f560b1460d8345ab5c9
gyojinnK <ksmug1998@gmail.com>
2024-03-24 18:57:14 +0900

    :memo: Update file
    contextProvider, 카테고리 슬라이드 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,20 @@ import topPUI from "./assets/images/topPhoneUI.png";
 import Article from "./components/article/Article.tsx";
 import Footer from "./components/footer/Footer.tsx";
 import Header from "./components/layouts/Header.tsx";
+import NavAllow from "./components/layouts/NavAllow.tsx";
+import { CategoryProvider } from "./stores/CategoryContext.tsx";
 
 const App: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
         <div className="mainWrap">
             <img src={topPUI} alt="topUI" />
-            <Header />
-            <Article />
-            {children}
-            <Footer />
+            <CategoryProvider>
+                <Header />
+                <NavAllow />
+                <Article />
+                {children}
+                <Footer />
+            </CategoryProvider>
         </div>
     );
 };
