0cdd84ed946ac0f1e0676769d7b3760c062387ab
gyojinnK <ksmug1998@gmail.com>
2024-03-24 19:01:21 +0900

    :memo: Update file
    각 페이지에 code splitting 적용 (lazy)

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,15 @@
+import { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ChartPage from "../pages/ChartPage";
-import WhookPage from "../pages/WhookPage";
-import EventPage from "../pages/EventPage";
-import NewsPage from "../pages/NewsPage";
-import StorePage from "../pages/StorePage";
-import ChargePage from "../pages/ChargePage";
-import App from "../App";
 import { QueryClient, QueryClientProvider } from "react-query";
+import App from "../App";
+import LoadSpinner from "../components/ui/LoadSpinner";
+
+const ChartPage = lazy(() => import("../pages/ChartPage"));
+const WhookPage = lazy(() => import("../pages/WhookPage"));
+const EventPage = lazy(() => import("../pages/EventPage"));
+const NewsPage = lazy(() => import("../pages/NewsPage"));
+const StorePage = lazy(() => import("../pages/StorePage"));
+const ChargePage = lazy(() => import("../pages/ChargePage"));
 
 const Router = () => {
     const queryClient = new QueryClient();
@@ -14,14 +17,27 @@ const Router = () => {
         <BrowserRouter>
             <App>
                 <QueryClientProvider client={queryClient}>
-                    <Routes>
-                        <Route path="/" element={<ChartPage />} />
-                        <Route path="/whook" element={<WhookPage />} />
-                        <Route path="/event" element={<EventPage />} />
-                        <Route path="/news" element={<NewsPage />} />
-                        <Route path="/store" element={<StorePage />} />
-                        <Route path="/charge" element={<ChargePage />} />
-                    </Routes>
+                    <Suspense
+                        fallback={
+                            <div
+                                style={{
+                                    width: "100%",
+                                    height: "100%",
+                                }}
+                            >
+                                <LoadSpinner />
+                            </div>
+                        }
+                    >
+                        <Routes>
+                            <Route path="/" element={<ChartPage />} />
+                            <Route path="/whook" element={<WhookPage />} />
+                            <Route path="/event" element={<EventPage />} />
+                            <Route path="/news" element={<NewsPage />} />
+                            <Route path="/store" element={<StorePage />} />
+                            <Route path="/charge" element={<ChargePage />} />
+                        </Routes>
+                    </Suspense>
                 </QueryClientProvider>
             </App>
         </BrowserRouter>
