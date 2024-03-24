73b17d04007b091d616fdcd88838e199df31a8f9
gyojinnK <ksmug1998@gmail.com>
2024-03-24 19:00:46 +0900

    :adhesive_bandage: Simple fix

--- src/pages/WhookPage.tsx
+++ src/pages/WhookPage.tsx
@@ -1,5 +1,5 @@
-import ListFrame from "../components/section/ListFrame";
 import whookDatas from "../db/whookInfo.json";
+import ListFrame from "../components/section/ListFrame";
 
 const WhookPage = () => {
     return <ListFrame mockData={whookDatas} categoryTitle="WHOOK" />;
